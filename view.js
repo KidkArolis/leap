7efc27a5e3dd3c0807fcaaf01abb38fa6e1374d9
Karolis Narkevicius <karolis.n@gmail.com>
2013-08-16 17:10:18 +0100

    Improve support for subviews
    * subviews can now be lazily created by
      leap view using factories provided in subviewCreators
    * createSubview replaces and renders subviews
    * renderSubview is an evolution of assign where
      passing a list of subview names is enough to render them

--- view.js
+++ view.js
@@ -28,10 +28,22 @@ define(function (require) {
 
       this.bindEvents();
 
+      // some place to keep track of which subviews have been rendered
       this._subviewsRendered = {};
+      // and what selectors for each subview's container were used
       this._subviewContainerSelectors = {};
     },
 
+
+
+    // Default render implementation
+    // -----------------------------
+
+    /**
+      Renders the template with template data, renders subviews, binds ui hash
+      to elements. This can be overriden, but it's advisable to use afterRender
+      to extend the default render behaviour.
+    */
     render: function () {
       if (this.destroyed) {
         return this;
@@ -46,16 +58,61 @@ define(function (require) {
       } else {
         html = "";
       }
-      this.$el.html(html);
-      _.each(this.subviews, function (view, name) {
-        this.assign("." + dashify(name) + "-container", name);
+
+      // detach the subviews first not to lose events
+      // as well as to keep their rendered DOM state
+      // the views should be responsible for rerendering themselves
+      // after model changes, etc.
+      eachNested(this._subviewsRendered, function (subview, subviewName) {
+        // only detach subviews that we know how to reattach - example
+        // don't detach modal dialogs rendered outside of the view's $el
+        // however, the subviews that have been rendered into $el without
+        // using the `renderSubviews` or `createSubviews` methods will
+        // be deattached and not reattached - avoid rendering ad hoc subviews
+        // yourself or reattach them in afterRender
+        if (this._subviewContainerSelectors[subviewName]) {
+          subview.$el.detach();
+        }
       }, this);
+
+      this.$el.html(html);
+
+      // before we render the subviews - bind the UI elements
+      this.bindUIElements();
+
+      // render this.subviews that haven't been rendered before
+      _.each(this.subviews, function (subview, subviewName) {
+        if (!this._subviewsRendered[subviewName]) {
+          this.renderSubviews(subviewName);
+        } else {
+          // if the view has been rendered, it means we detached it
+          // and so we need to reattach, but only if it's not been
+          // destroyed already
+          var destroyed = true;
+          eachNested([subview], function (s) {
+            destroyed && s.destroyed;
+          });
+          // also, only reattach if we have a selector for this view,
+          // so for example if a modal subview has been rendered into the body
+          // we didn't detach it - so no need to reattach.
+          var selector = this._subviewContainerSelectors[subviewName];
+          if (!destroyed && selector) {
+            this._renderSubview(selector, subviewName, {
+              skipRender: true
+            });
+          }
+        }
+      }, this);
+
+      // TODO: remove dependency on stickit
       if (this.bindings) {
         this.stickit();
       }
-      this.bindUIElements();
+
       this.afterRender();
+      // TODO: investigate why this event is needed, can we avoid having it?
       this.trigger("afterRender");
+      
       return this;
     },
 
@@ -72,33 +129,20 @@ define(function (require) {
         model: this.model,
         collection: this.collection,
         cid: this.cid,
-        linkTo: _.bind(this.linkTo, this)
+        linkTo: _.bind(this.linkTo, this),
+        subview: _.bind(this._subviewHelper, this)
       };
     },
 
-    // a way to generate links, delegates to router.generate
-    linkTo: function () {
-      if (mediator.router) {
-        return mediator.router.generate.apply(mediator.router, arguments);
-      }
-    },
-
-    transitionTo: function () {
-      if (mediator.router) {
-        return mediator.router.transitionTo.apply(mediator.router, arguments);
-      }
-    },
-
-    replaceWith: function () {
-      if (mediator.router) {
-        return mediator.router.replaceWith.apply(mediator.router, arguments);
-      }
-    },
-
+    /**
+      a hook for passing more data to the template
+    */
     templateData: function () {},
 
-    // This method binds the elements specified in the "ui" hash inside the
-    // view's code with the associated jQuery selectors.
+    /**
+      This method binds the elements specified in the "ui" hash inside the
+      view's code with the associated jQuery selectors.
+    */
     bindUIElements: function () {
       if (!this.ui) { return; }
 
@@ -118,6 +162,11 @@ define(function (require) {
         that.ui[key] = that.$(selector);
       });
     },
+
+
+
+    // Model and Collection events
+    // ---------------------------
 
     bindEvents: function () {
       this.bindBackboneEntityTo(this.model, this.modelEvents);
@@ -142,50 +191,46 @@ define(function (require) {
       });
     },
 
-    assign: function (selector, view) {
-      var selectors;
-      var instance = this;
-      if (_.isObject(selector)) {
-        selectors = selector;
-      } else {
-        selectors = {};
-        selectors[selector] = view;
-      }
-      if (!selectors) {
-        return;
-      }
-      _.each(selectors, function (view, selector) {
-        var viewName;
-        if (_.isString(view)) {
-          viewName = view;
-          view = instance.subviews[viewName];
-        }
-        if (!view) {
-          throw new Error("Can't assign subview '" + viewName +
-            "', because it does not exist.");
-        }
 
-        // don't render if there is no container for it
-        if (!this.$(selector).length) {
-          return;
-        }
 
-        if (view instanceof Backbone.View) {
-          // call setElement to redelegate events
-          this.$(selector).html(view.setElement(view.$el).render().el);
-        } else {
-          // this.$(selector).empty();
-          _.each(view, function (view) {
-            // call setElement to redelegate events
-            this.$(selector).append(view.setElement(view.$el).render().el);
+    // Subviews
+    // --------
+
+    // subview constructors that will be consumed by render
+    // when it's time to render in subviews
+    subviewCreators: null,
+
+    /**
+      (re)renders a subview or many subviews into their containers
+      e.g.
+      render("someSubview");
+      render("subview1", "subview2");
+      render(["subview1", "subview2"]);
+      render({
+        ".custom-container-class": "subview1
+      });
+    */
+    renderSubviews: function () {
+      var subviewNames = _.flatten(_.toArray(arguments));
+      _.each(subviewNames, function (subviewName) {
+        if (_.isObject(subviewName)) {
+          // this is a hash with selectors -> subview names
+          _.each(subviewName, function (subviewName, selector) {
+            this._renderSubview(selector, subviewName);
           }, this);
+        } else {
+          // this is a subview name
+          this._renderSubview(this._subviewNameToSelector(subviewName), subviewName);
         }
       }, this);
     },
 
     /**
-      Replaces a subview with a new instance, but makes sure the old instance
-      is destroyed first.
+      Creates a new subview or replaces an existing one with a new instance,
+      but first makes sure the old instance is destroyed. It automatically renders
+      the new subview if container exists in the $el. Pass render: false to not
+      render.
+
       Useful for subviews that are created and destroyed several times throughout
       the lifecycle of some view (e.g. modal dialogs, dropdowns)
 
@@ -194,18 +239,61 @@ define(function (require) {
       var settings = new SettingsModal({
         model: this.model
       });
-      this.replaceSubview("settings", settings);
-      this.assign(".settings-container", "settings");
+      this.createSubview("settings", settings);
+
+      var modal = new Modal();
+      this.createSubview("modal", modal, { render: false });
+      $("body").append(modal.render().el);
+
+      // this actually destroys the subview, and because no new subview
+      // is passed in - it stays destroyed
+      this.createSubview("search");
     */
-    replaceSubview: function (subviewName, newSubviewInstance) {
+    createSubview: function (subviewName, newSubviewInstance, options) {
+      options = _.extend({
+        render: true
+      }, options);
+
       if (this.subviews[subviewName]) {
         this.subviews[subviewName].destroy();
         delete this.subviews[subviewName];
       }
+
       if (newSubviewInstance) {
         this.subviews[subviewName] = newSubviewInstance;
+        if (options.render) {
+          this.renderSubviews(subviewName);
+        }
       }
     },
+
+
+    
+    // Cherrytree router helpers
+    // -------------------------
+
+
+    // a way to generate links, delegates to router.generate
+    linkTo: function () {
+      if (mediator.router) {
+        return mediator.router.generate.apply(mediator.router, arguments);
+      }
+    },
+
+    // a way for views to transition to other routes
+    transitionTo: function () {
+      if (mediator.router) {
+        return mediator.router.transitionTo.apply(mediator.router, arguments);
+      }
+    },
+
+    // just like transitionTo, but replaces the current URL in browser history
+    replaceWith: function () {
+      if (mediator.router) {
+        return mediator.router.replaceWith.apply(mediator.router, arguments);
+      }
+    },
+
 
 
     // Disposal
@@ -233,15 +321,9 @@ define(function (require) {
       // a loop)
 
       // Destroy subviews. Handle arrays and individual views
-      _.each(this.subviews, function (subview) {
+      eachNested(this.subviews, function (subview) {
         if (subview) {
-          if (_.isArray(subview)) {
-            _.invoke(subview, "destroy");
-          } else if ($.isPlainObject(subview)) {
-            _.invoke(_.values(subview), "destroy");
-          } else {
-            subview.destroy();
-          }
+          subview.destroy();
         }
       });
 
@@ -275,7 +357,97 @@ define(function (require) {
       }
     },
 
-    beforeDestroy: function () {}
+    // a hook for custom destroy code to be executed in addition
+    // to all of the default destroy functionality. Good place for unbinding
+    // events or destroying non leap views (e.g. jquery plugins), etc.
+    beforeDestroy: function () {},
+
+    
+    // private methods
+
+    // can be used in templates for generating container divs
+    _subviewHelper: function (subviewName) {
+      return "<div class='" + this._subviewNameToClass(subviewName) + "'></div>";
+    },
+
+    _renderSubview: function (selector, view, options) {
+      options = _.extend({
+        skipRender: false
+      }, options);
+
+      var $container = this.$(selector);
+      // don't render if there is no container for this view
+      // in $el's dom
+      if (!$container.length) {
+        return;
+      }
+
+      var viewName;
+      if (_.isString(view)) {
+        viewName = view;
+        view = this.subviews[viewName];
+      }
+      if (!view) {
+        throw new Error("Subview '" + viewName + "' doesn't exist");
+      }
+
+      // now that we have a view instance, or array/object of view instances
+      // render and inject them all into their container
+      eachNested([view], function (v, key, inner) {
+        if (!options.skipRender) {
+          v.setElement(v.$el).render();
+        }
+        var injector = inner ? "append" : "html";
+        $container[injector](v.el);
+      });
+
+      // keep the record of which views we've rendered - to avoid rerendering
+      this._subviewsRendered[viewName] = view;
+      // and into what containers we've rendered them - for reattaching
+      this._subviewContainerSelectors[viewName] = selector;
+    },
+
+    // instantiates a subview if it hasn't been done before
+    _ensureSubviewCreated: function (subviewName) {
+      // only initialize the subview if it hasn't been already rendered or initialized
+      // also only initialize if the container is available in the DOM
+      if (!this.subviews[subviewName] && this._subviewContainer(subviewName).length) {
+        if (this.subviewCreators[subviewName]) {
+          this.subviews[subviewName] = this.subviewCreators[subviewName].apply(this);
+        } else {
+          throw new Error("LeapView: subview creator for", subviewName, "doesn't exist");
+        }
+      }
+    },
+
+    _subviewContainer: function (subviewName) {
+      return this.$(this._subviewNameToSelector(subviewName));
+    },
+
+    _subviewNameToSelector: function (subviewName) {
+      return "." + this._subviewNameToClass(subviewName);
+    },
+
+    _subviewNameToClass: function (subviewName) {
+      return dashify(subviewName) + "-container";
+    },
   });
+
+  // loops over each key/value of an object, and if value is array/plainObject, loops
+  // over those as well
+  // callback args are:
+  // the value, the outer object's key and an inner boolean flag
+  // i.e. true if the callback is called from within the nested array/object
+  function eachNested(obj, fn, context) {
+    _.each(obj, function (val, key) {
+      if (_.isArray(val) || $.isPlainObject(val)) {
+        _.each(val, function (nestedVal) {
+          fn.call(context, nestedVal, key, true);
+        });
+      } else {
+        fn.call(context, val, key, false);
+      }
+    });
+  }
 
 });
